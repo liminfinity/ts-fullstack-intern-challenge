1ad13476655330cb40cf5796732a8e99a49cd0ac
liminfinity <108136720+liminfinity@users.noreply.github.com>
2024-09-17 21:44:49 +0700

    chore: edit eslint

---
+++ api/eslint.config.js
@@ -0,0 +1,23 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+    ],
+    files: ['**/*.{ts,tsx}'],
+    ignores: ['dist'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.node,
+    },
+    rules: {
+      'no-console': 'warn',
+    },
+  },
+  eslintConfigPrettier,
+);
