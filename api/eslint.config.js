6e68e1549907a6ea19679d0b2039fe17f76d1c5a
liminfinity <108136720+liminfinity@users.noreply.github.com>
2024-09-17 23:25:48 +0700

    chore: add husky and lint-staged

--- api/eslint.config.js
+++ api/eslint.config.js
@@ -5,11 +5,8 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-    ],
-    files: ['**/*.{ts,tsx}'],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.ts'],
     ignores: ['dist'],
     languageOptions: {
       ecmaVersion: 2020,
